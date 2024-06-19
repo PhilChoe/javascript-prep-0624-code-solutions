d06f291c84ba11a138a10e3a18f09f75a0b16787
Philip J Choe <j12choe@gmail.com>
2024-06-19 05:32:37 +0000

    Completed java op and exp

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var number1 = 2;
+var number2 = 4;
+prod = number1 * number2;
+console.log(2 * 4);
+console.log(typeof prod);
+
+var charge = 10;
+var payment = 5;
+amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+var tests = 1;
+var assignments = 2;
+var finals = 3;
+grade = (tests + assignments + finals) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Philip';
+const lastName = 'Choe';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+var pH = 2;
+isAcidic = pH < 7;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+var numberOfSoldiers = 100;
+isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log(nickname);
+console.log(typeof nickname);
