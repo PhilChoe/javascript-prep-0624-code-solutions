f59cbc49cf3fd321cd5735b0709a3f1b40f66cdc
Philip J Choe <j12choe@gmail.com>
2024-06-20 04:41:58 +0000

    Changed var to const javascript-operators-and-expressions

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -1,18 +1,18 @@
-var number1 = 2;
-var number2 = 4;
+const number1 = 2;
+const number2 = 4;
 prod = number1 * number2;
 console.log(2 * 4);
 console.log(typeof prod);
 
-var charge = 10;
-var payment = 5;
+const charge = 10;
+const payment = 5;
 amountRemaining = charge - payment;
 console.log(amountRemaining);
 console.log(typeof amountRemaining);
 
-var tests = 1;
-var assignments = 2;
-var finals = 3;
+const tests = 1;
+const assignments = 2;
+const finals = 3;
 grade = (tests + assignments + finals) / 3;
 console.log(grade);
 console.log(typeof grade);
@@ -23,12 +23,12 @@ const fullName = firstName + ' ' + lastName;
 console.log(fullName);
 console.log(typeof fullName);
 
-var pH = 2;
+const pH = 2;
 isAcidic = pH < 7;
 console.log(isAcidic);
 console.log(typeof isAcidic);
 
-var numberOfSoldiers = 100;
+const numberOfSoldiers = 100;
 isSparta = numberOfSoldiers === 300;
 console.log(isSparta);
 console.log(typeof isSparta);
